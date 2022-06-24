5c90f32b7dc00271399f27473b61bd83fa2a087b
catlejuyeon <87415326+catlejuyeon@users.noreply.github.com>
2022-06-24 12:33:23 +0900

    30

---
+++ profileapp/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')     #on_delete user가 삭제될때 casecade, 프로필도 같이 삭제
+
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=20, unique=True, null=True)
+    message = models.CharField(max_length=100, null=True)
+
+    
