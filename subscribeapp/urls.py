75f3610e590d2c72801221eef4e94f8086a4549b
catlejuyeon <87415326+catlejuyeon@users.noreply.github.com>
2022-06-28 17:45:38 +0900

    subscribeapp/field lookup을 사용하여  구독페이지 구현 43

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from subscribeapp.views import SubscriptionView
+from subscribeapp.views import SubscriptionView, SubScriptionListView
 
 app_name = 'subscribeapp'
 
 urlpatterns = [
     path('subscribe/', SubscriptionView.as_view(), name='subscribe'),
+    path('list/', SubScriptionListView.as_view(), name='list'),
 ]
