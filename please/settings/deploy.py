08e247b5ec0ce294c60959988cfbec621f6986cc
catlejuyeon <87415326+catlejuyeon@users.noreply.github.com>
2022-10-11 23:43:48 +0900

    https 다시수정3

--- please/settings/deploy.py
+++ please/settings/deploy.py
@@ -25,7 +25,7 @@ environ.Env.read_env(
 SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://happyplease.shop']     #forbidden 오류csrf...
